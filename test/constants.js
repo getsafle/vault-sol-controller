bef5bb0df8775d4d58e321fc0d985f37893c9ef1
SDargarh <sdargarh26@gmail.com>
2024-07-22 17:40:39 +0530

    Initial Solana Keyring Implementation

---
+++ test/constants.js
@@ -0,0 +1,19 @@
+module.exports = {
+    HD_WALLET_12_MNEMONIC: 'affair entry detect broom axis crawl found valve bamboo taste broken hundred',
+    
+    SOLANA_NETWORK: {
+        MAINNET: {
+            NETWORK: "MAINNET",
+            URL: 'https://api.mainnet-beta.solana.com',
+        },
+
+        TESTNET: {
+            NETWORK: "TESTNET",
+            URL: 'https://api.testnet.solana.com',
+        },
+        DEVNET: {
+            NETWORK: "DEVNET",
+            URL: 'https://api.devnet.solana.com'
+        }
+    },
+}
